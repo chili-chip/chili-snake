5258a235338c91001d23072b005f734bf0d8bb86
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2026-02-25 20:48:15 +0100

    add game over

--- game.cpp
+++ game.cpp
@@ -7,7 +7,7 @@ using namespace blit;
 #define Y_LIMIT 128/SCALE_FACTOR
 
 Point segments[1024] = {Point(16,16)};
-int size = 1;
+int snake_size = 1;
 Point food_position = Point(10,10);
 
 enum direction{
@@ -18,20 +18,32 @@ enum direction{
 };
 
 direction current_direction = UP;
+direction input_direction = UP;
 
 uint32_t timer = 0;
 
+bool game_over = false;
+
+void reset_game(){
+    segments[0] = Point(16,16);
+    snake_size = 1;
+    food_position = Point(10,10);
+    current_direction = UP;
+    timer = 0;
+    game_over = false;
+}
+
 void draw_snake(){
     screen.pen = Pen(0,255,0);
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < snake_size; i++){
         screen.rectangle(Rect(segments[i].x*SCALE_FACTOR, segments[i].y*SCALE_FACTOR, SCALE_FACTOR, SCALE_FACTOR));
     }
 }
 
 void grow_snake(){
-    if(size < 1024){
-        segments[size] = segments[size-1];
-        size++;
+    if(snake_size < 1024){
+        segments[snake_size] = segments[snake_size-1];
+        snake_size++;
     }
 }
 
@@ -42,7 +54,7 @@ void draw_food(){
 
 void spawn_food(){
     Point new_position = Point(rand() % X_LIMIT, rand() % Y_LIMIT);
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < snake_size; i++){
         if (segments[i] == new_position){
             spawn_food();
             return;
@@ -58,8 +70,17 @@ void check_food(){
     }
 }
 
+void check_game_over(){
+    for (int i = 1; i < snake_size; i++){
+        if (segments[0] == segments[i]){
+            game_over = true;
+            return;
+        }
+    }
+}
+
 void move_snake(){
-    for (int i = size-1; i > 0; i--){
+    for (int i = snake_size-1; i > 0; i--){
         segments[i] = segments[i-1];
     }
 
@@ -93,17 +114,35 @@ void move_snake(){
 
 void get_input(){
     if (buttons & Button::DPAD_UP){
-        current_direction = UP;
+        if (current_direction != DOWN){
+            input_direction = UP;
+        }
     }
     else if (buttons & Button::DPAD_DOWN){
-        current_direction = DOWN;
+        if (current_direction != UP){
+            input_direction = DOWN;
+        }
     }
     else if (buttons & Button::DPAD_RIGHT){
-        current_direction = RIGHT;
+        if (current_direction != LEFT){
+            input_direction = RIGHT;
+        }
     }
     else if (buttons & Button::DPAD_LEFT){
-        current_direction = LEFT;
+        if (current_direction != RIGHT){
+            input_direction = LEFT;
+        }
     }
+
+    if (buttons & Button::A){
+        if (game_over){
+            reset_game();
+        }
+    }
+}
+
+void update_direction(){
+    current_direction = input_direction;
 }
 
 void init() {
@@ -113,18 +152,22 @@ void init() {
 void render(uint32_t time) {
     screen.pen = Pen(0,0,0);
     screen.clear();
+    if (game_over) {
+        screen.pen = Pen(255,255,255);
+        screen.text("Game Over", minimal_font, Point(64, 64), true, TextAlign::center_center);
+        return;
+    }
     draw_food();
     draw_snake();
 }
 
 void update(uint32_t time) {
-
     get_input();
-    
     if (time - timer > 400){
+        update_direction();
         check_food();
         move_snake();
+        check_game_over();
         timer = time;
     }
-
 }
